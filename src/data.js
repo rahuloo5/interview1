b1db65b6bb40a8d651a719f7bf66453862d27aff
rahuloo5 <rahultomar092@gmail.com>
2020-12-20 18:29:02 +0530

    interview

---
+++ src/data.js
@@ -0,0 +1,22 @@
+export const data = 
+[
+
+{
+    id:1,
+    name:"Rahul SIngh Tomar",
+    email :"rahultomar092@gmail.com",
+    mobile :8319968379,
+    address :"gwalior"
+
+},
+
+{
+    id:1,
+    name:"Vikas SIngh Tomar",
+    email :"vikas@gmail.com",
+    mobile :831996837,
+    address :"Delhi"
+
+}
+
+]
--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {ProfileProvider} from './Context'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 ReactDOM.render(
-  <React.StrictMode>
+ 
+ <ProfileProvider>
     <App />
-  </React.StrictMode>,
+    </ProfileProvider>
+  ,
   document.getElementById('root')
 );
 
